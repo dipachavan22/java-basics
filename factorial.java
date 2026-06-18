08e7a08887af251f271badc4f6497b31ea7a4038
dipachavan22 <dipachavan2210@gmail.com>
2026-06-18 22:06:43 +0530

    Added Prime Number program

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class factorial
+{
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number to find its factorial:");
+    int n=sc.nextInt();
+
+    int fact=1;
+    for(int i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    System.out.println("Factorial of "+n+" is: "+fact);
+}
+}
---
+++ fibonacci.java
@@ -0,0 +1,6 @@
+import java.io.*;
+import java.util.*;
+class fibonacci
+{
+  public static void main(String arg[])
+}
